f9e8485a6f503d0fd38a68859a67a5908004067d
Huzaifa Rashid <huzaifaras10@gmail.com>
2026-06-07 13:00:06 -0800

    Allow firebase admin sdk initialization using FIREBASE_SERVICE_ACCOUNT environment variable

--- server/config/firebase.js
+++ server/config/firebase.js
@@ -1,8 +1,29 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('./firebase-service-account.json');
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
-});
+let serviceAccount;
+
+if (process.env.FIREBASE_SERVICE_ACCOUNT) {
+  try {
+    serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+  } catch (err) {
+    console.error('Failed to parse FIREBASE_SERVICE_ACCOUNT environment variable:', err);
+  }
+}
+
+if (!serviceAccount) {
+  try {
+    serviceAccount = require('./firebase-service-account.json');
+  } catch (err) {
+    console.warn('Could not find firebase-service-account.json locally. Ensure FIREBASE_SERVICE_ACCOUNT environment variable is set.');
+  }
+}
+
+if (serviceAccount) {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+  });
+} else {
+  console.error('Firebase Admin SDK failed to initialize: No service account credentials available.');
+}
 
 module.exports = admin;
